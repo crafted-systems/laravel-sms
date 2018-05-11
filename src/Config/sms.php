ec9a2326997ed93d1b82983435fd3c5d865af3ac
Vincent Mosoti <vincent@vmosoti.com>
2018-05-11 10:27:41 +0300

    add support for dynamic base url for micromobile api

--- src/Config/sms.php
+++ src/Config/sms.php
@@ -38,7 +38,8 @@ return [
             'service_id' => env('MICROMOBILE_SERVICE_ID'),
             'username' => env('MICROMOBILE_USERNAME'),
             'password' => env('MICROMOBILE_PASSWORD'),
-            'call_back_url' => env('MICROMOBILE_CALL_BACK_URL')
+            'call_back_url' => env('MICROMOBILE_CALL_BACK_URL'),
+            'base_url' => env('MICROMOBILE_BASE_URL'),
         ],
 
         'africastalking' => [
